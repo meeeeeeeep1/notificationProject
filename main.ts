86e7f51a669e63795a20123d0e12f7d06311bc03
meeeeeeeep1 <75493196+meeeeeeeep1@users.noreply.github.com>
2023-07-16 08:04:12 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+let message = "randin"
+let encoded = Buffer.fromBase64(message)
+control.simmessages.send("notify", encoded)
+control.simmessages.onReceived("notify",(msg: Buffer) => {
+console.log(msg.toBase64())
+}
+)
